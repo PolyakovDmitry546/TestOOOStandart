812005f055e2ee92d81e3d539a4d14824b3dc787
PolyakovDmitry546 <65275577+PolyakovDmitry546@users.noreply.github.com>
2024-01-24 19:10:16 +0500

    Add payments view take 1

---
+++ src/payments/views.py
@@ -0,0 +1,13 @@
+from typing import Any
+from django.db.models.query import QuerySet
+from django.views.generic.list import ListView
+
+from payments.models import Invoice
+
+
+class InvoiceListView(ListView):
+    model = Invoice
+    paginate_by = 5
+
+    def get_queryset(self) -> QuerySet[Any]:
+        return Invoice.objects.all().order_by('id').select_related('requisite')
