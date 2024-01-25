938abd14b5532ed17146064e80d6551dedf1b233
PolyakovDmitry546 <65275577+PolyakovDmitry546@users.noreply.github.com>
2024-01-25 15:58:19 +0500

    Add search for requisite by any field

--- src/payments/utils.py
+++ src/payments/utils.py
@@ -1,10 +1,12 @@
 from dataclasses import dataclass
 
+from payments.models import Requisite
+
 
 @dataclass
 class TableHeader:
+    verbose_name: str
     name: str
-    ordering: str
 
 
 def get_requisite_table_headers():
@@ -16,3 +18,8 @@ def get_requisite_table_headers():
         TableHeader('Номер телефона', 'phone'),
         TableHeader('Лимит', 'limit'),
     ]
+
+
+def get_requisite_model_fields() -> list[str]:
+    field_names = [field.name for field in Requisite._meta.get_fields()]
+    return field_names
--- src/payments/views.py
+++ src/payments/views.py
@@ -1,10 +1,16 @@
 from collections.abc import Sequence
 from typing import Any
+
 from django.db.models.query import QuerySet
+from django.forms import ValidationError
+from django.http import HttpResponse, HttpResponseNotFound
 from django.views.generic.list import ListView
 
 from payments.models import Invoice, Requisite
-from payments.utils import TableHeader, get_requisite_table_headers
+from payments.utils import (
+    get_requisite_table_headers,
+    get_requisite_model_fields,
+)
 
 
 class InvoiceListView(ListView):
@@ -20,12 +26,62 @@ class RequisiteListView(ListView):
     paginate_by = 50
     ordering = 'id'
 
+    def validate_query_params(self):
+        ordering = self.request.GET.get('ordering')
+        if ordering is not None:
+            if len(ordering) > 0 and ordering[0] == '-':
+                ordering = ordering[1:]
+            if ordering not in get_requisite_model_fields():
+                raise ValidationError(
+                    f'Incorrect ordering parameter: ordering={ordering}'
+                )
+        search_field = self.request.GET.get('search_field')
+        if search_field is not None and \
+                search_field not in get_requisite_model_fields():
+            raise ValidationError(
+                f'Incorrect search_field parameter: search_field={search_field}'
+            )
+
+    def get(self, request, *args, **kwargs) -> HttpResponse:
+        try:
+            self.validate_query_params()
+        except ValidationError as e:
+            return HttpResponseNotFound(content=e.message)
+        return super().get(request, *args, **kwargs)
+
+    def get_queryset(self) -> QuerySet[Any]:
+        search_field = self.get_search_field()
+        search_value = self.get_search_value()
+        query = super().get_queryset()
+        if search_field is None or search_value is None:
+            return query
+
+        search_field += "__icontains"
+        query_kwargs = {search_field: search_value}
+        query = query.filter(**query_kwargs)
+        return query
+
     def get_context_data(self, **kwargs: Any) -> dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context['ordering'] = self.get_ordering()
         context['table_headers'] = get_requisite_table_headers()
+
+        search_field = self.get_search_field()
+        if search_field is not None:
+            context['search_field'] = search_field
+
+        search_value = self.get_search_value()
+        if search_value is not None:
+            context['search_value'] = self.get_search_value()
+
         return context
 
     def get_ordering(self) -> Sequence[str]:
-        ordering = self.request.GET.get('ordering')
+        ordering = self.request.GET.get('ordering', self.ordering)
         return ordering
+
+    def get_search_field(self) -> str | None:
+        return self.request.GET.get('search_field')
+
+    def get_search_value(self) -> str | None:
+        return self.request.GET.get('search_value')
