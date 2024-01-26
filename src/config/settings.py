0f7084db258700025f09492756dc38497a94cdd5
PolyakovDmitry546 <65275577+PolyakovDmitry546@users.noreply.github.com>
2024-01-26 22:34:04 +0500

    Install django rest framework

--- src/config/settings.py
+++ src/config/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    "phonenumber_field",
+    'rest_framework',
+    'phonenumber_field',
     'django_seed',
 
     'payments',
