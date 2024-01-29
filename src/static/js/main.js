d0262a3afb78e51432bb53be5f43540bfd7c8519
PolyakovDmitry546 <65275577+PolyakovDmitry546@users.noreply.github.com>
2024-01-29 13:25:17 +0500

    Create ajax request for ordering

--- src/static/js/main.js
+++ src/static/js/main.js
@@ -1,29 +1,111 @@
-async function orderingFormSubmit(event) {
-    event.preventDefault();
-    const form = event.target;
-    let requestUrl = form.action;
-    let ordering = form.elements["ordering"].value;
+function getOrderingInput(orderingForm) {
+    return orderingForm.elements["ordering"];
+}
+
+function getOrderingSubmit(orderingForm) {
+    let orderingSubmit;
+    for (let elem of orderingForm.elements) {
+        if (elem.type === "submit") orderingSubmit = elem;
+    }
+    return orderingSubmit;
+}
+
+function getNewOrderingFromForm(orderingForm) {
+    return getOrderingInput(orderingForm).value;
+}
+
+function reverseOrdering(ordering) {
     if (ordering[0] === "-") {
         ordering = ordering.slice(1);
     }
     else {
         ordering = "-" + ordering;
     };
-    //changeOrdering()
-    requisiteTableFilterParameters.set("ordering", ordering);
+    return ordering;
+}
+
+function getRequestUrl(orderingFrom) {
     const queryParams = new URLSearchParams();
     requisiteTableFilterParameters.forEach((value, key) => {
         if (value !== null) queryParams.append(key, value);
     });
     const queryString = queryParams.toString();
+    let requestUrl = orderingFrom.action;
     requestUrl = requestUrl + "?" + queryString;
+    return requestUrl;
+}
+
+async function sendRequest(requestUrl) {
     const response = await fetch(requestUrl, {
         headers: {
             "x-requested-with": "XMLHttpRequest"
         }
     }).catch(reason => console.log(reason));
-    response_data = await response.json();
-    //updateTable(response_data)
+    const responseData = await response.json();
+    return responseData;
+}
+
+function getOrderingSubmitValue(previousValue, ordering) {
+    let value;
+    if (previousValue.startsWith( "▼") || previousValue.startsWith("▲")) {
+        value = previousValue.slice(1);
+    }
+    else {
+        value = previousValue;
+    }
+    if (ordering.startsWith("-")) {
+        value = "▼" + value;
+    }
+    else {
+        value = "▲" + value;
+    }
+    return value;
+}
+
+function updateOrderingFormInputs(orderingForm, ordering) {
+    const orderingInput = getOrderingInput(orderingForm);
+    const orderingSubmit = getOrderingSubmit(orderingForm);
+
+    orderingSubmit.value = getOrderingSubmitValue(orderingSubmit.value, ordering);
+    orderingInput.value = reverseOrdering(ordering);
+}
+
+function updateTableData(requisiteList) {
+    const table = document.getElementById("requisite-table");
+    const tBody = document.createElement("tbody");
+    for (let requisite of requisiteList) {
+        let tr = document.createElement("tr");
+        tr.classList.add("table-info");
+        let i = 0;
+        for (let field in requisite) {
+            let tElem;
+            if (i === 0) {
+                tElem = document.createElement('th');
+                tElem.setAttribute("scope", "row");
+                i++;
+            }
+            else {
+                tElem = document.createElement('td');
+            }
+            tElem.innerText = requisite[field];
+            tr.append(tElem);
+        }
+        tBody.append(tr);
+    }
+    const oldTBody = table.getElementsByTagName("tbody")[0];
+    table.replaceChild(tBody, oldTBody);
+}
+
+async function orderingFormSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    
+    let ordering = getNewOrderingFromForm(form);
+    requisiteTableFilterParameters.set("ordering", ordering);
+    const requestUrl = getRequestUrl(form);
+    const responseData = await sendRequest(requestUrl);
+    updateOrderingFormInputs(form, ordering);
+    updateTableData(responseData.object_list);
     //updateNavigation(response_data)
 }
 
