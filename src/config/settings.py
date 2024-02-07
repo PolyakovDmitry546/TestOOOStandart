3c9ec2b2b5ac75771d59a12eb88b9e84afd65911
PolyakovDmitry546 <65275577+PolyakovDmitry546@users.noreply.github.com>
2024-02-07 16:38:53 +0500

    Replace sqlite with postgresql

--- src/config/settings.py
+++ src/config/settings.py
@@ -84,9 +84,13 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test',
+        'USER': 'pguser',
+        'PASSWORD': 'pgpassword',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    },
 }
 
 
